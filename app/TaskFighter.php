224d36a54e32fc9261afa76535b303b1836106b5
Bernard vd Merwe <barny.vdm@gmail.com>
2020-08-08 22:11:59 +0200

    updated task 3, finished task 4

--- app/TaskFighter.php
+++ app/TaskFighter.php
@@ -23,48 +23,112 @@ class TaskFighter
 
     public function tick()
     {
-        if ($this->name != 'Get Older') {
-            if ($this->priority < 100) {
-                if ($this->name != 'Breathe') {
-                    $this->priority = $this->priority + 1;
-                }
-            }
-            if ($this->name == 'Complete Assessment') {
-                if ($this->dueIn < 11) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
-                    }
-                }
-                if ($this->dueIn < 6) {
-                    if ($this->priority < 100) {
-                        $this->priority = $this->priority + 1;
-                    }
-                }
-            }
+        if ($this->name == 'Breathe') {
+            return $this->handle_breathe();
+        }
+        if ($this->name == 'Get Older') {
+            return $this->handle_get_older();
+        }
+        if ($this->name == 'Complete Assessment') {
+            return $this->handle_complete_assesment();
+        }
+
+        /* Handle regular tasks */
+
+        $this->handle_normal_task();
+
+        // if ($this->name != 'Get Older') {
+        //     if ($this->priority < 100) {
+        //         if ($this->name != 'Breathe') {
+        //             $this->priority = $this->priority + 1;
+        //         }
+        //     }
+        //     if ($this->name == 'Complete Assessment') {
+        //         if ($this->dueIn < 11) {
+        //             if ($this->priority < 100) {
+        //                 $this->priority = $this->priority + 1;
+        //             }
+        //         }
+        //         if ($this->dueIn < 6) {
+        //             if ($this->priority < 100) {
+        //                 $this->priority = $this->priority + 1;
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     if ($this->priority > 0) {
+        //         $this->priority = $this->priority - 1;
+        //     }
+        // }
+        // if ($this->name != 'Breathe') {
+        //     $this->dueIn = $this->dueIn - 1;
+        // }
+        // if ($this->dueIn < 0) {
+        //     if ($this->name != 'Get Older') {
+        //         if ($this->name != 'Complete Assessment') {
+        //             if ($this->priority < 100) {
+        //                 if ($this->name != 'Breathe') {
+        //                     $this->priority = $this->priority + 1;
+        //                 }
+        //             }
+        //         } else {
+        //             $this->priority = $this->priority - $this->priority;
+        //         }
+        //     } else {
+        //         if ($this->priority > 0) {
+        //             $this->priority = $this->priority - 1;
+        //         }
+        //     }
+        // }
+
+    }
+
+    private function handle_get_older() {
+        //negate 2 from priority so that it negates one when processed
+        $this->priority -= 2;
+        $this->handle_normal_task();
+    }
+
+    private function handle_breathe() {
+        //- "Breathe", being something that just happens, never has to be completed or increase in priority
+        // "Breathe" is an automatic task and as such its priority is 1000 and it never alters.
+        $this->priority = 1000;
+    }
+
+    private function handle_complete_assesment() {
+        //- "Complete Assessment" increases in priority as it's dueIn value approaches
+        //Priority increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but priority drops to 0 after the due date.
+
+        if ($this->dueIn > 10) {
+            $this->handle_normal_task();
+        } elseif ($this->dueIn <= 10 && $this->dueIn > 5) {
+            $this->priority += 1;
+            $this->handle_normal_task();
+        } elseif ($this->dueIn <= 5 && $this->dueIn > 3) {
+            $this->priority += 2;
+            $this->handle_normal_task();
         } else {
-            if ($this->priority > 0) {
-                $this->priority = $this->priority - 1;
-            }
+            $this->priority = -2;
+            $this->handle_normal_task();
         }
-        if ($this->name != 'Breathe') {
-            $this->dueIn = $this->dueIn - 1;
-        }
+    }
+
+    private function handle_normal_task() {
+        // - At the end of each day our system lowers the dueIn value
+        $this->dueIn -= 1;
+        // and increases the priority value for every item
+        $this->priority += 1;
+        // - Once the due date has passed, priority increases twice as fast
         if ($this->dueIn < 0) {
-            if ($this->name != 'Get Older') {
-                if ($this->name != 'Complete Assessment') {
-                    if ($this->priority < 100) {
-                        if ($this->name != 'Breathe') {
-                            $this->priority = $this->priority + 1;
-                        }
-                    }
-                } else {
-                    $this->priority = $this->priority - $this->priority;
-                }
-            } else {
-                if ($this->priority > 0) {
-                    $this->priority = $this->priority - 1;
-                }
-            }
+            $this->priority += 1;
+        }
+        // - The priority of an item is never negative
+        if ($this->priority < 0) {
+            $this->priority = 0;
+        }
+        // - The priority of an item is never more than 100
+        if ($this->priority > 100) {
+            $this->priority = 100;
         }
     }
 }
--- tests/Unit/TaskFighterTest.php
+++ tests/Unit/TaskFighterTest.php
@@ -30,15 +30,15 @@ class TaskFighterTest extends TestCase
     public function test_taskfighter_tick_breathe() {
         fwrite(STDERR, print_r(__FUNCTION__.PHP_EOL, TRUE));
         $name = 'Breathe';
-        $dueIn = 30;
-        $priority = 1000;
+        $dueIn = 300;
+        $priority = 200;
         $tf = new TaskFighter($name, $priority, $dueIn);
 
         $tf->tick();
 
         $this->assertTrue($tf->name == $name, __FUNCTION__.' name');
         $this->assertTrue($tf->dueIn == $dueIn, __FUNCTION__.' dueIn');
-        $this->assertTrue($tf->priority == $priority, __FUNCTION__.' priority');
+        $this->assertTrue($tf->priority == 1000, __FUNCTION__.' priority');
     }
     /**
      * Test if Taskfighter tick works as expected with Get Older 
@@ -133,20 +133,20 @@ class TaskFighterTest extends TestCase
         $priority = 50;
         $tf = new TaskFighter($name, $priority, $dueIn);
 
-        $this->test_taskfighter_tick_normal($tf);
+        $this->taskfighter_tick_normal($tf);
         $tf->priority = -1;
-        $this->test_taskfighter_tick_normal_negative_priority($tf);
+        $this->taskfighter_tick_normal_negative_priority($tf);
 
         $tf->dueIn = -1;
-        $this->test_taskfighter_tick_normal_passed_due($tf);
+        $this->taskfighter_tick_normal_passed_due($tf);
 
         //increase priority a lot
         for ($i = 0; $i < 100; $i++) {
             $tf->tick();
         }
-        $this->test_taskfighter_tick_normal_priority_over_100($tf);
+        $this->taskfighter_tick_normal_priority_over_100($tf);
     }
-    public function test_taskfighter_tick_normal($tf) {
+    public function taskfighter_tick_normal($tf) {
         fwrite(STDERR, print_r(__FUNCTION__.PHP_EOL, TRUE));
 
         $old_tf = new TaskFighter($tf->name, $tf->priority, $tf->dueIn);
@@ -157,7 +157,7 @@ class TaskFighterTest extends TestCase
         $this->assertTrue($tf->dueIn == $old_tf->dueIn - 1, __FUNCTION__.' dueIn');
         $this->assertTrue($tf->priority == $old_tf->priority + 1, __FUNCTION__.' priority');
     }
-    public function test_taskfighter_tick_normal_negative_priority($tf) {
+    public function taskfighter_tick_normal_negative_priority($tf) {
         fwrite(STDERR, print_r(__FUNCTION__.PHP_EOL, TRUE));
 
         $old_tf = new TaskFighter($tf->name, $tf->priority, $tf->dueIn);
@@ -168,7 +168,7 @@ class TaskFighterTest extends TestCase
         $this->assertTrue($tf->dueIn == $old_tf->dueIn - 1, __FUNCTION__.' dueIn');
         $this->assertTrue($tf->priority >= 0, __FUNCTION__.' priority');
     }
-    public function test_taskfighter_tick_normal_passed_due($tf) {
+    public function taskfighter_tick_normal_passed_due($tf) {
         fwrite(STDERR, print_r(__FUNCTION__.PHP_EOL, TRUE));
 
         $old_tf = new TaskFighter($tf->name, $tf->priority, $tf->dueIn);
@@ -179,7 +179,7 @@ class TaskFighterTest extends TestCase
         $this->assertTrue($tf->dueIn == $old_tf->dueIn - 1, __FUNCTION__.' dueIn');
         $this->assertTrue($tf->priority == $old_tf->priority + 2, __FUNCTION__.' priority');
     }
-    public function test_taskfighter_tick_normal_priority_over_100($tf) {
+    public function taskfighter_tick_normal_priority_over_100($tf) {
         fwrite(STDERR, print_r(__FUNCTION__.PHP_EOL, TRUE));
 
         $old_tf = new TaskFighter($tf->name, $tf->priority, $tf->dueIn);
